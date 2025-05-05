473138b1ad3f32bc34f6e8566a33a134c52f1bae
David Jin <tdavidjin@gmail.com>
2025-05-05 18:39:43 +0200

    added cifar100

---
+++ setup_cifar100.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# setup_cifar100.sh
+
+# Create CIFAR-100 directory
+mkdir -p data/cifar-100-python
+
+echo "CIFAR-100 directory created at data/cifar-100-python"
+echo "You can now use the --dataset CIFAR100 parameter to run experiments with CIFAR-100"
