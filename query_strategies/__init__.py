34da0ef658e7944f0a97e2bd172499083414c716
David <tdavidjin@gmail.com>
2025-09-07 10:59:10 +0200

    removed all test strategies. same random seeding

--- query_strategies/__init__.py
+++ query_strategies/__init__.py
@@ -1,5 +1,3 @@
-# Import all available sampling strategies and manager
-
 from .badge import BADGESampler
 from .entropy import EntropySampler
 from .kafal import KAFALSampler
@@ -7,12 +5,10 @@ from .random import RandomSampler
 from .noise_stability import NoiseStabilitySampler
 from .feal import FEALSampler
 from .logo import LoGoSampler
-from .strategy_manager import StrategyManager
-from .entropy_global_optimal import ClassBalancedEntropySampler
 from .coreset import CoreSetSampler
-from .coreset_global_optimal import ClassBalancedCoreSetSampler
-from .pseudo_confidence import PseudoClassBalancedConfidenceSampler
 from .pseudo_entropy_variance import PseudoClassBalancedVarianceEntropySampler
+from .ablation_class_uncertainty import AblationClassUncertaintySampler
+from .strategy_manager import StrategyManager
 
 # Define __all__ for explicit exports
 __all__ = [
@@ -24,10 +20,7 @@ __all__ = [
     "NoiseStabilitySampler",
     "FEALSampler",
     "LoGoSampler",
-    "ClassBalancedEntropySampler",
+    "AblationClassUncertaintySampler",
     "CoreSetSampler",
-    "ClassBalancedCoreSetSampler",
-    "PseudoClassBalancedEntropySampler",
-    "PseudoClassBalancedConfidenceSampler",
     "PseudoClassBalancedVarianceEntropySampler",
 ]
